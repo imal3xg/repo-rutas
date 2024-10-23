e5caf742fe395cda233e5406b4df5fe8b692f9f1
Alejandro Giráldez Guerrero <agirgue659@cpifpalanturing.es>
2024-10-23 13:45:26 +0200

    first commit

---
+++ src/app/shared/components/person-modal/person-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person-modal',
+  templateUrl: './person-modal.component.html',
+  styleUrls: ['./person-modal.component.scss'],
+})
+export class PersonModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
